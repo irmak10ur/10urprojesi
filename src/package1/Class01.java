047645176ea53cda06ab9ad5da2251ae97ec72e5
OnuR <irmak10ur@gmail.com>
2020-06-20 21:31:17 +0200

    conflict olusturma

--- src/package1/Class01.java
+++ src/package1/Class01.java
@@ -4,6 +4,9 @@ public class Class01 {
 
 	public static void main(String[] args) {
 		System.out.println("my commit");
+		System.out.println("my commitimiz 2.olmus");
+		System.out.println("my commit 3");
+		
 	}
 
 }
